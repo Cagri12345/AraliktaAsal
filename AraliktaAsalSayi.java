697e643fc6cfe415a89018a708243f1447d99a15
ÇağrıTuğrulKeser <162361570+Cagri12345@users.noreply.github.com>
2024-07-31 02:00:17 +0300

    AraliktaAsalSayi
    1-100 arasındaki asal sayıları ekrana yazdıran java kodu

---
+++ AraliktaAsalSayi.java
@@ -0,0 +1,21 @@
+package Basic;
+public class AraliktaAsalSayi {
+    public static void main(String[] args) {
+        System.out.println("1 - 100 arasındaki asal sayılar:");
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
